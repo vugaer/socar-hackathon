822dcdfd36d9c296336cfcfa46a3b6fbf5dfa783
Vugar Dunyamaliyev <vugar.dunyamaliyev.std@bhos.edu.az>
2026-01-22 13:43:37 +0400

    gitpusher

---
+++ solutions/corrupted_parquet.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""This module is preferably done in Python. In bash format
+it is not recommended either. It is possible to create bash
+file write stuff to a file and then run it using python3 then
+locate path using Path module. So yes, it is worth doing on Python"""
+
+import sys
